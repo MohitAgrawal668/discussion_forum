fc29ba40b9b24e076cd0199ca974f82025658128
Mohit Agrawal <agrawalm668@gmail.com>
2023-06-15 16:02:17 +0530

    created models Channel and Discussion

--- database/migrations/2023_06_15_101047_create_channels_table.php
+++ database/migrations/2023_06_15_101047_create_channels_table.php
@@ -13,11 +13,8 @@ return new class extends Migration
     {
         Schema::create('channels', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id');
-            $table->string('title');
-            $table->text('content');
-            $table->string('slug');
-            $table->integer('channel_id');
+            $table->string('name');
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
--- database/migrations/2023_06_15_101056_create_discussions_table.php
+++ database/migrations/2023_06_15_101056_create_discussions_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('discussions', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
+            $table->string('title');
+            $table->text('content');
+            $table->string('slug')->unique();
+            $table->integer('channel_id');
             $table->timestamps();
         });
     }
